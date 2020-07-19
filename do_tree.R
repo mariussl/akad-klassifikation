698ca46cac1f03913a6e8dc200131efc89170cef
marius <marius.lessiak@schubs.at>
2020-07-19 19:38:54 +0200

    bugfixes, working pruning of tree

--- do_tree.R
+++ do_tree.R
@@ -1,10 +1,16 @@
 library(data.tree)
-df <- read.csv("testdata.csv")
-df <- transform(df, Streckenverlauf = as.factor(Streckenverlauf), Streckenqualitaet = as.factor(Streckenqualitaet), Wind = (Wind == "Ja"), Startzeit = as.factor(Startzeit), Sieg = (Sieg == "Ja"))
+#df <- read.csv("testdata.csv")
+#df <- transform(df, Streckenverlauf = as.factor(Streckenverlauf), Streckenqualitaet = as.factor(Streckenqualitaet), Wind = (Wind == "Ja"), Startzeit = as.factor(Startzeit), Sieg = (Sieg == "Ja"))
+df <- read.csv("data.csv")
+df <- transform(df, Bundesland = as.factor(Bundesland), Abschluss = as.factor(Abschluss), Stellung = as.factor(Stellung), Geschlecht = as.factor(Geschlecht), Wohngebiet = as.factor(Wohngebiet), Eigenheim = (Eigenheim == "j"), Familienstand = as.factor(Familienstand), MitgliedSportverein = (MitgliedSportverein == "j"), Preiskategorie = as.factor(Preiskategorie))
+set.seed(1765)
+sample_idx <- sample.int(nrow(df), round(nrow(df)*0.2))
+test <- df[sample_idx,]
+train <- df[-sample_idx,]
 calcEntropy <- function(ctable) {
   ctable_freq <- apply(ctable,2,function(x){x/sum(x)})
   ctable_entropy <- apply(ctable_freq, 2,function(x){ ifelse(x!=0, x * log2(x), 0)})
-  entropy_sum <- sum(colSums(ctable_entropy) * -1)
+  entropy_sum <- sum(colSums(ctable_entropy, na.rm = TRUE) * -1)
   return (entropy_sum)
 }
 isHomogen <- function(v) {
@@ -28,18 +34,21 @@ getNodeId <- function() {
 }
 buildTree <- function(df, targetProp, classProps) {
   nodeId <- getNodeId()
+  if (nodeId == "Knoten_9") {
+    print("Ml")
+  }
   thisNode <- Node$new(nodeId)
   thisNode$id <- nodeId
   thisNode$data <- df
   thisNode$targetFrequDist <- getFrequDist(table(df[targetProp]))
   thisNode$label <- sprintf("%s|%s", thisNode$id, paste(sprintf("%.2f", thisNode$targetFrequDist), sep = "", collapse = " "))
+  thisNode$error <- getErrorCount(table(df[targetProp]))
+  thisNode$total <- getTotalCount(table(df[targetProp]))
+  thisNode$label <- sprintf("%s|Vorhersage %s|Total_Richtig_Falsch %d_%d_%d", 
+                            thisNode$label, names(which(table(df[targetProp]) == max(table(df[targetProp]))))[1], 
+                            thisNode$total, (thisNode$total - thisNode$error), 
+                            thisNode$error)
   if ((length(classProps) == 0) || isHomogen(table(df[targetProp]))) {
-    thisNode$error <- getErrorCount(table(df[targetProp]))
-    thisNode$total <- getTotalCount(table(df[targetProp]))
-    thisNode$label <- sprintf("%s|Vorhersage %s|Total_Richtig_Falsch %d_%d_%d", 
-                              thisNode$label, names(table(df[targetProp])), 
-                              thisNode$total, (thisNode$total - thisNode$error), 
-                              thisNode$error)
     SetNodeStyle(thisNode, shape = "record", label=sprintf("{%s}", thisNode$label ))
     return (thisNode)
   }
@@ -50,7 +59,7 @@ buildTree <- function(df, targetProp, classProps) {
     ctable <- table(df[, targetProp], df[, classProp])
     classPropsEntropy[classProp] <- calcEntropy(ctable)
   }
-  useClassProp <- colnames(classPropsEntropy)[which(classPropsEntropy==min(classPropsEntropy))]
+  useClassProp <- colnames(classPropsEntropy)[which(classPropsEntropy==min(classPropsEntropy))][1]
   thisNode$label <- sprintf("%s|div by %s", thisNode$label, useClassProp)
   newClassProps <- classProps[!(classProps %in% useClassProp)]
   for (classPropValue in unique(df[, useClassProp])) {
@@ -60,4 +69,30 @@ buildTree <- function(df, targetProp, classProps) {
   SetNodeStyle(thisNode, shape = "record", label=sprintf("{%s}", thisNode$label ))
   return (thisNode)
 }
-tree <- buildTree(df, "Sieg", c("Streckenverlauf", "Streckenqualitaet", "Wind", "Startzeit"))
+pruneTree <- function(tree) {
+  if (tree$isLeaf) {
+    return (tree)
+  }
+  subtreeError <- 0
+  for (childNode in tree$children) {
+    pruneTree(childNode)
+    subtreeError <- subtreeError + childNode$error
+  }
+  if (subtreeError >= tree$error) {
+    for (childNode in tree$children) {
+      tree$RemoveChild(childNode$id)
+    }
+  } else {
+    # replace error count with that from subtree, else tree is replaced by parent node 
+    # by looking at incorrect performance
+    tree$error <- subtreeError
+  }
+  return (tree)
+}
+#tree <- buildTree(df, "Sieg", c("Streckenverlauf", "Streckenqualitaet", "Wind", "Startzeit"))
+tree <- buildTree(train, "Preiskategorie", c("Abschluss", "Geschlecht" , "Wohngebiet" , "Stellung", 
+                  "Eigenheim", "Familienstand","Bundesland", "MitgliedSportverein"))
+#tree <- buildTree(train, "Preiskategorie", c("Bundesland", "MitgliedSportverein"))
+ptree <- pruneTree(Clone(tree))
+#print(ptree, "label")
+#plot(tree)
